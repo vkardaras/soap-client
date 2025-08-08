7a98d4c002997a4ed60eaefaa139b771e49a961e
vkardaras <vasiliskardaras@gmail.com>
2025-08-08 12:31:44 +0300

    Add security

--- src/main/java/com/vasiliskardaras/soap_client/CountryConfiguration.java
+++ src/main/java/com/vasiliskardaras/soap_client/CountryConfiguration.java
@@ -5,6 +5,7 @@ import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 
 @Configuration
@@ -23,6 +24,8 @@ public class CountryConfiguration {
     public CountryClient countryClient(Jaxb2Marshaller marshaller) {
         CountryClient client = new CountryClient();
         client.setDefaultUri("http://localhost:8080/ws");
+        ClientInterceptor[] interceptors = new ClientInterceptor[] {securityInterceptor()};
+        client.setInterceptors(interceptors);
         client.setMarshaller(marshaller);
         client.setUnmarshaller(marshaller);
         return client;
@@ -33,7 +36,7 @@ public class CountryConfiguration {
         Wss4jSecurityInterceptor security = new Wss4jSecurityInterceptor();
 
         // Adds "Timestamp" and "UsernameToken" sections in SOAP header
-        security.setSecurementActions(WSHandlerConstants.TIMESTAMP + " " + WSHandlerConstants.USERNAME_TOKEN);
+        security.setSecurementActions(WSHandlerConstants.USERNAME_TOKEN);
 
         // Set values for "UsernameToken" sections in SOAP header
         security.setSecurementPasswordType(WSConstants.PW_TEXT);
